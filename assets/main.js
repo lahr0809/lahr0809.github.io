f0642ea5f7f06970218a043a32dcc46f1b689ebf
Luis Alberto Hernandez <lahr0809@gmail.com>
2023-03-26 08:56:18 -0500

    solution count channels

--- assets/main.js
+++ assets/main.js
@@ -1,30 +1,27 @@
-
 const url = 'https://viper.jugacosenamas.store/categorias';
-
-
 const recibeInfo = () => {
     fetch(url)
         .then(response => response.json())
         .then(datos => {
-            let cad = ``;
-          
+            let cad = ``;   
             for (let usuario of datos.data) {
                 let contador = 0;
                 cad += `<div class="accordionItem">
                 <div class="accordionTitle" id="${usuario.id}" onClick="myFunction(${usuario.id})">
-                <div class="hora_canales">${usuario.horario}</div>
+                <div class="hora_canales"><span>Horario</span><span>${usuario.horario}</span></div>
                 <div class="juego">
                     <span>${usuario.nombre}</span>
                     <h3>${usuario.titulo}</h3>
                     </div>
-                   <div class="hora_canales">`;
+                   <div class="num_canales">`;
                     
                   for (let urls of usuario.canales) {
                     if (urls.id != 0) contador++;
-               cad += `<p>${contador} Canales</p>`;
                 };
-                   
-                   cad += `</div>
+                   cad += `<svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z">
+                   </path>
+               </svg><br>${contador} Canales</div>
                   </div>
                   <div class="accordionContent">
                   <div>`;
@@ -38,8 +35,6 @@ const recibeInfo = () => {
                   cad +=`</div>
                 </div>
               </div>`;
-
-
             };
          
             document.getElementById("content-table").innerHTML = cad;
