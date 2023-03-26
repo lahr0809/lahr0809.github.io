c28a91fdd8ae968db6e569d6114e90005c0fdb02
Luis Alberto Hernandez <lahr0809@gmail.com>
2023-03-25 19:41:15 -0500

    add accordion

--- assets/main.js
+++ assets/main.js
@@ -4,29 +4,51 @@ const url = 'https://viper.jugacosenamas.store/categorias';
 
 const recibeInfo = () => {
     fetch(url)
-            .then(response => response.json())
-            .then(datos => {
-                let cad = '<tr><th>Competición</th><th>Encuentro</th><th>Canal</th></tr>';
-               
-                for (let usuario of datos.data) {
-                    cad += `<tr><td>${usuario.nombre}</td>
-                                <td>${usuario.titulo}</td><td>`;   
-                                                
-                                for (let urls of usuario.canales) {
-                                    const newStr = urls.url_video.slice(7)
-                                    cad +=`<button onclick="loadChanel('${newStr}')">${urls.titulo}</button>`;
-                                };
-                                
+        .then(response => response.json())
+        .then(datos => {
+            let cad = ``;
+          
+            for (let usuario of datos.data) {
+                let contador = 0;
+                cad += `<div class="accordionItem">
+                <div class="accordionTitle" id="${usuario.id}" onClick="myFunction(${usuario.id})">
+                <div class="hora_canales">${usuario.horario}</div>
+                <div class="juego">
+                    <span>${usuario.nombre}</span>
+                    <h3>${usuario.titulo}</h3>
+                    </div>
+                   <div class="hora_canales">`;
+                    
+                  for (let urls of usuario.canales) {
+                    if (urls.id != 0) contador++;
+               cad += `<p>${contador} Canales</p>`;
+                };
+                   
+                   cad += `</div>
+                  </div>
+                  <div class="accordionContent">
+                  <div>`;
+                
+                  for (let urls of usuario.canales) {
+                    const newStr = urls.url_video.slice(7);
+              
+               contador++;
+                    cad += `<button onclick="loadChanel('${newStr}')">${urls.titulo}</button>`;
+                };
+                  cad +=`</div>
+                </div>
+              </div>`;
 
-                }
-                cad += `</td></tr>`;
-                document.getElementById("list").innerHTML = cad;
-            });
+
+            };
+         
+            document.getElementById("content-table").innerHTML = cad;
+        });
 }
-const loadChanel = (chanel)=>{
+const loadChanel = (chanel) => {
     document.getElementById("iframe").src = "https://" + chanel;
- }
- function boton_full_screen() {
+}
+function boton_full_screen() {
     document.getElementById("boton_full_screen").style =
         "display:none";
     document.getElementById(
